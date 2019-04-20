ca4afb34d98adf3b75fa3f4e7abbf913aee4de38
Paola Pajaro <yindypaolapu@ufps.edu.co>
2019-04-19 21:29:12 -0500

    Ultimos cambios

--- Inventario/Views/Login.Designer.cs
+++ Inventario/Views/Login.Designer.cs
@@ -33,8 +33,8 @@
             this.chkCajero = new System.Windows.Forms.CheckBox();
             this.chkAdministrador = new System.Windows.Forms.CheckBox();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblPass = new System.Windows.Forms.Label();
+            this.lblCedula = new System.Windows.Forms.Label();
             this.txtCedula = new System.Windows.Forms.TextBox();
             this.txtPassword = new System.Windows.Forms.TextBox();
             this.helpProvider1 = new System.Windows.Forms.HelpProvider();
@@ -43,9 +43,10 @@
             // 
             // btnIngresar
             // 
-            this.btnIngresar.Location = new System.Drawing.Point(240, 209);
+            this.btnIngresar.Location = new System.Drawing.Point(180, 170);
+            this.btnIngresar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnIngresar.Name = "btnIngresar";
-            this.btnIngresar.Size = new System.Drawing.Size(75, 27);
+            this.btnIngresar.Size = new System.Drawing.Size(56, 22);
             this.btnIngresar.TabIndex = 0;
             this.btnIngresar.Text = "Ingresar";
             this.btnIngresar.UseVisualStyleBackColor = true;
@@ -53,9 +54,10 @@
             // 
             // btnSalir
             // 
-            this.btnSalir.Location = new System.Drawing.Point(240, 260);
+            this.btnSalir.Location = new System.Drawing.Point(180, 211);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnSalir.Name = "btnSalir";
-            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.Size = new System.Drawing.Size(56, 19);
             this.btnSalir.TabIndex = 1;
             this.btnSalir.Text = "Salir";
             this.btnSalir.UseVisualStyleBackColor = true;
@@ -64,9 +66,10 @@
             // chkCajero
             // 
             this.chkCajero.AutoSize = true;
-            this.chkCajero.Location = new System.Drawing.Point(325, 155);
+            this.chkCajero.Location = new System.Drawing.Point(244, 126);
+            this.chkCajero.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.chkCajero.Name = "chkCajero";
-            this.chkCajero.Size = new System.Drawing.Size(71, 21);
+            this.chkCajero.Size = new System.Drawing.Size(56, 17);
             this.chkCajero.TabIndex = 2;
             this.chkCajero.Text = "Cajero";
             this.chkCajero.UseVisualStyleBackColor = true;
@@ -75,18 +78,19 @@
             // chkAdministrador
             // 
             this.chkAdministrador.AutoSize = true;
-            this.chkAdministrador.Location = new System.Drawing.Point(175, 155);
+            this.chkAdministrador.Location = new System.Drawing.Point(131, 126);
+            this.chkAdministrador.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.chkAdministrador.Name = "chkAdministrador";
-            this.chkAdministrador.Size = new System.Drawing.Size(117, 21);
+            this.chkAdministrador.Size = new System.Drawing.Size(89, 17);
             this.chkAdministrador.TabIndex = 3;
             this.chkAdministrador.Text = "Administrador";
             this.chkAdministrador.UseVisualStyleBackColor = true;
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.PaleGreen;
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.label1);
+            this.panel1.BackColor = System.Drawing.Color.IndianRed;
+            this.panel1.Controls.Add(this.lblPass);
+            this.panel1.Controls.Add(this.lblCedula);
             this.panel1.Controls.Add(this.txtCedula);
             this.panel1.Controls.Add(this.txtPassword);
             this.panel1.Controls.Add(this.chkAdministrador);
@@ -95,54 +99,61 @@
             this.panel1.Controls.Add(this.btnIngresar);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(572, 348);
+            this.panel1.Size = new System.Drawing.Size(429, 283);
             this.panel1.TabIndex = 4;
             // 
-            // label2
+            // lblPass
             // 
-            this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Location = new System.Drawing.Point(172, 116);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(69, 17);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "Password";
+            this.lblPass.AutoSize = true;
+            this.lblPass.BackColor = System.Drawing.Color.Transparent;
+            this.lblPass.Location = new System.Drawing.Point(129, 94);
+            this.lblPass.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblPass.Name = "lblPass";
+            this.lblPass.Size = new System.Drawing.Size(53, 13);
+            this.lblPass.TabIndex = 7;
+            this.lblPass.Text = "Password";
             // 
-            // label1
+            // lblCedula
             // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Location = new System.Drawing.Point(172, 83);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(52, 17);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "Cedula";
-            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            this.lblCedula.AutoSize = true;
+            this.lblCedula.BackColor = System.Drawing.Color.Transparent;
+            this.lblCedula.Location = new System.Drawing.Point(129, 67);
+            this.lblCedula.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblCedula.Name = "lblCedula";
+            this.lblCedula.Size = new System.Drawing.Size(40, 13);
+            this.lblCedula.TabIndex = 6;
+            this.lblCedula.Text = "Cedula";
+            this.lblCedula.Click += new System.EventHandler(this.Label1_Click);
             // 
             // txtCedula
             // 
-            this.txtCedula.Location = new System.Drawing.Point(296, 83);
+            this.txtCedula.Location = new System.Drawing.Point(222, 67);
+            this.txtCedula.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtCedula.Name = "txtCedula";
-            this.txtCedula.Size = new System.Drawing.Size(100, 22);
+            this.txtCedula.Size = new System.Drawing.Size(76, 20);
             this.txtCedula.TabIndex = 5;
             this.txtCedula.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(296, 111);
+            this.txtPassword.Location = new System.Drawing.Point(222, 90);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtPassword.Name = "txtPassword";
-            this.txtPassword.Size = new System.Drawing.Size(100, 22);
+            this.txtPassword.PasswordChar = '°';
+            this.txtPassword.Size = new System.Drawing.Size(76, 20);
             this.txtPassword.TabIndex = 4;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(572, 348);
+            this.ClientSize = new System.Drawing.Size(429, 283);
             this.Controls.Add(this.panel1);
             this.ForeColor = System.Drawing.SystemColors.Desktop;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "InventarioSoft";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -153,17 +164,16 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btnIngresar;
-        private System.Windows.Forms.Button btnSalir;
-        private System.Windows.Forms.CheckBox chkCajero;
-        private System.Windows.Forms.CheckBox chkAdministrador;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox txtCedula;
-        private System.Windows.Forms.TextBox txtPassword;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.HelpProvider helpProvider1;
+        public System.Windows.Forms.Button btnIngresar;
+        public System.Windows.Forms.Button btnSalir;
+        public System.Windows.Forms.CheckBox chkCajero;
+        public System.Windows.Forms.CheckBox chkAdministrador;
+        public System.Windows.Forms.Label lblCedula;
+        public System.Windows.Forms.TextBox txtCedula;
+        public System.Windows.Forms.TextBox txtPassword;
+        public System.Windows.Forms.Label lblPass;
     }
 }
 
